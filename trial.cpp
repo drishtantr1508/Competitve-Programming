10a23a4fc2b4f0c11d82288103db6b4d3da99577
Drishtant Rai <drishtant.rai.min19@itbhu.ac.in>
2021-07-25 12:59:50 +0530

    ...

---
+++ trial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<string>
+#include<algorithm>
+#include<numeric>
+#include<set>
+#define int long long
+#define ll long long
+using namespace std;
+
+void solve(){
+	//code here
+}
+
+signed main()
+{
+	cout<<"Trial File"<<endl;
+}
